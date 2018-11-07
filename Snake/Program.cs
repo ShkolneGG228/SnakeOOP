416700d3031f4ff2497a0d37da974a358fc852c0
ShkolneGG228 <moget.prenko@mail.ru>
2018-11-08 00:01:24 +0300

    Function

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -9,8 +9,16 @@ namespace Snake
     class Program
     {
         static void Main(string[] args)
+        {           
+            int x1 = 1;
+            int y1 = 3;char sym1 = '*';
+            Draw(x1, y1, sym1);
+            Console.ReadKey();
+        }
+        static void Draw(int x,int y,char sym)
         {
-            Console.WriteLine("Hello");
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
