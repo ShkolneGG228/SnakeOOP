fb3fd16d9533b8b47035578223de2d35e2f47636
ShkolneGG228 <moget.prenko@mail.ru>
2018-11-08 16:20:02 +0300

    Snake

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,28 +10,12 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');          
-            p1.Draw();
-           
-            Point p2 = new Point(4,5,'%');
-            p2.Draw();
+            
+            HorizontalLine lineHor = new HorizontalLine(5,10,8,'$');
+            lineHor.Drow();
 
-            List<int> numList = new List<int>();
-            numList.Add(0); numList.Add(1); numList.Add(2);
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
+            VerticalLine lineVer = new VerticalLine(5, 10, 18, '%');
+            lineVer.Drow();
 
             Console.ReadKey();
         }
